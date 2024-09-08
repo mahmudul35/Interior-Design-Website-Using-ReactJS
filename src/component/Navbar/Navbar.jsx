8c29ac38aa9ec88b33c6a4aede7378a97d36be7b
mahmudul35 <101060541+mahmudul35@users.noreply.github.com>
2024-09-08 00:52:58 -0700

    project setup and navbar added

---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Logo from "../../assets/logo-1.png";
+const Navbar = () => {
+  const NavLinks = [
+    {
+      id: 1,
+      title: "About",
+      link: "#",
+    },
+    {
+      id: 2,
+      title: "Service",
+      link: "#",
+    },
+    {
+      id: 3,
+      title: "Project",
+      link: "#",
+    },
+    {
+      id: 4,
+      title: "Contact",
+      link: "#",
+    },
+  ];
+  return (
+    <div className="container py-3 flex justify-between items-center">
+      {/* logo */}
+      <div className="flex items-center gap-1 capitalize">
+        <img src={Logo} alt="pic" className="w-10" />
+        <span className="font-extrabold text-2xl ">Interior</span>
+      </div>
+      {/* link */}
+      <div className=" hidden md:flex">
+        {NavLinks.map((link) => {
+          return (
+            <a className="mx-3 font-semibold lg:text-base" href={link.link}>
+              {link.title}
+            </a>
+          );
+        })}
+      </div>
+      {/* button */}
+      <div>
+        <button className="border border-black px-3 py-2 shadow-[5px_5px_0px_0px] hover:bg-black hover:text-white transition ease-in-out">
+          Try For Free
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
